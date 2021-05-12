2a9f1f13bfce95302a22a2dd918dfcf3a1f661f3
Rachel Estilo <rachelestilo@me.com>
2021-05-12 03:01:14 -0500

    Slideshow, menu, and foot

---
+++ js/app-min.js
@@ -0,0 +1,2 @@
+"use strict";$(document).foundation(),$((function(){$("#nav-placeholder").load("nav.html")})),$((function(){$("#foot-placeholder").load("foot.html")}));
+//# sourceMappingURL=app-min.js.map
