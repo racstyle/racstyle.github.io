6756033fb04e36f67bf70ec0cd8adddd0d12dd01
Rachel Estilo <raystilo4@gmail.com>
2023-04-05 01:08:01 -0500

    More actual content added + tweaks

--- js/app.js
+++ js/app.js
@@ -5,15 +5,28 @@ $(document).foundation();
 var navHeight = document.querySelector('.sticky-shrinknav-header').offsetHeight;
 document.documentElement.style.setProperty('--scroll-pad', navHeight + 'px');
 
-// Dynamic shrinking nav/header container
+// Scroll to top button via id
+let myButton = document.getElementById("scrollTop");
+
+// Dynamic shrinking nav/header container + show/hide scroll to top button
 $(function() {
   $(window).on('scroll', function() {
     var winTop = $(window).scrollTop();
+    // User scrolled down
     if (winTop >= 30) {
       $("body").addClass("sticky-shrinknav-wrapper");
+      myButton.style.display = "block";
     }
+    // User scrolled to top
     else  {
       $("body").removeClass("sticky-shrinknav-wrapper");
+      myButton.style.display = "none";
     }
   });
 });
+
+// Scrolling to top function
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
