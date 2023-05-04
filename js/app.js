3b65fcc0c6832ea9d11b7517dc0cd195dda3fc54
Rachel Estilo <raystilo4@gmail.com>
2023-05-04 16:45:41 -0500

    pushing before deleting old stuff

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
 $(document).foundation();
 
 // Scroll padding-top values when nav scrolling to section
-const navHeightHero = 396;
-const navHeightMenu = 70;
+// const navHeightHero = 396;
+// const navHeightMenu = 70;
 
 // Scroll to top button via id
 let myButton = document.getElementById("scrollTop");
@@ -12,8 +12,8 @@ let myButton = document.getElementById("scrollTop");
 // Main script
 $(function() {
   // TODO: TEMP To keep main content's padding-top constant to maintain nav position when scrolling (see TODO below)
-  var navHeight = document.querySelector('.sticky-shrinknav-header').offsetHeight;
-  document.documentElement.style.setProperty('--scroll-pad', navHeight + 'px');
+  // var navHeight = document.querySelector('.sticky-shrinknav-header').offsetHeight;
+  // document.documentElement.style.setProperty('--scroll-pad', navHeight + 'px');
   
   // Dynamic shrinking nav/header container + show/hide scroll to top button
   $(window).on('scroll', function() {
@@ -23,13 +23,13 @@ $(function() {
     // TODO: dynamic padding-top when scrolling via 'document.documentElement.style....' in if-else statements is conflicting with scroll nav positioning (just need to change the conditions?)
     // User scrolled down
     if (winTop >= 30) {
-      $("body").addClass("sticky-shrinknav-wrapper");     // hide the header/hero, menu bar only
+      // $("body").addClass("sticky-shrinknav-wrapper");     // hide the header/hero, menu bar only
       // document.documentElement.style.setProperty('--scroll-pad', navHeightMenu + 'px');    // set content small padding-top
       myButton.style.display = "block";                   // show "scroll to top" button
     }
     // User scrolled to top
     else  {
-      $("body").removeClass("sticky-shrinknav-wrapper");  // show the header/hero
+      // $("body").removeClass("sticky-shrinknav-wrapper");  // show the header/hero
       // document.documentElement.style.setProperty('--scroll-pad', navHeightHero + 2 + 'px');  // set content big padding-top
       myButton.style.display = "none";                    // hide "scroll to top" button
     }
@@ -38,8 +38,8 @@ $(function() {
 
 // Scrolling to top function
 function topFunction() {
-  document.documentElement.style.setProperty('--scroll-pad', navHeightHero + 'px');
-  document.body.classList.remove("sticky-shrinknav-wrapper");  // show the header/hero
+  // document.documentElement.style.setProperty('--scroll-pad', navHeightHero + 'px');
+  // document.body.classList.remove("sticky-shrinknav-wrapper");  // show the header/hero
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
