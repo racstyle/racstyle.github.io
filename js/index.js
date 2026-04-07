cafbe6088356a6f1476d5c5d8dd3aa1e12c61dd5
Rachel Gijon <raystilo4@gmail.com>
2026-04-07 18:19:21 -0500

    REG - page now scrolls to clicked nav-item + navbar highlights nav-item of current section scrolled to

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 /* --------------------------------- Navbar --------------------------------- */
 // #region Navbar
+// toggle menu for mobile
 const toggleButton = document.querySelector('.nav-toggle');
 const navLinks = document.querySelector('.nav-links');
 
@@ -18,6 +19,31 @@ document.querySelectorAll('.nav-links a').forEach(link => {
     // toggleButton.setAttribute('aria-expanded', false);
   });
 });
+
+
+// active nav item when scrolling to that section
+const sections = document.querySelectorAll('section[id]');  // select all sections with an id (to identify them for the nav links)
+
+// event listener for scroll to update active nav link
+window.addEventListener('scroll', () => {
+  const scrollY = window.pageYOffset + 20;  // current scroll position + navbar offset
+
+  // loop through each section to check if it's in the viewport
+  sections.forEach(section => {
+    const sectionHeight = section.offsetHeight;  // height of the section
+    const sectionTop = section.offsetTop - 50;  // top position of the section (adjusted for navbar height)
+    const sectionId = section.getAttribute('id');  // id of the section
+
+    // check if the current scroll position is within the section
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav-links a[href*=' + sectionId + ']').classList.add('active');  // add 'active' class to the corresponding nav link
+    }
+    else {
+      document.querySelector('.nav-links a[href*=' + sectionId + ']').classList.remove('active');  // remove 'active' class when not in the section
+    }
+  });
+});
+
 
 // #endregion Navbar
 
